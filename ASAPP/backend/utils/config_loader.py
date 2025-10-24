213f327f4f0e143ad943f6b1f8122e080e1623a8
Firoj Raut <firojraut094@gmail.com>
2025-10-24 09:50:39 +0545

    update config

--- ASAPP/backend/utils/config_loader.py
+++ ASAPP/backend/utils/config_loader.py
@@ -8,12 +8,18 @@ def load_config(path="../../config.yml"):
     try:
         absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), path))
         with open(absolute_path, "r") as file:
+            config = yaml.safe_load(file)
+
+            backend_url = os.getenv("BACKEND_URL")
+            if backend_url:
+                config["api"]["create_project"] = f"{backend_url}/create_project"
+                config["api"]["chat"] = f"{backend_url}/chat"
 
             logger.log("INFO", "Loading Config", {"message": "Config loaded Successfully."})
-            return yaml.safe_load(file)
+            return config
 
     except FileNotFoundError:
-        logger.log("INFO", "Config not found.", {"message": "Config file not found."})
+            logger.log("INFO", "Config not found.", {"message": "Config file not found."})
 
     except yaml.YAMLError as e:
         logger.log("ERROR", "Config Parse Error", {"error": str(e)})
